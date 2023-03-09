ca49943b8fe7ecfa35f9ebbfea93b2f4d102f42a
Roberto <robertog.adm@gmail.com>
2023-03-09 20:36:27 -0300

    Adicionando conteúdo do site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Home from './home';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={ Home }/>
-        <Route exact path="/about" component={ About }/>
+        {/* <Route exact path="/about" component={ About }/> */}
       </Switch>
     </div>
   );
--- src/home.jsx
+++ src/home.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 function Home() {
   return (
-  <h1>Home</h1>
+    <>
+    <h1>Homeaaa</h1>
+    <p>asdça</p>
+    </>
   );
 }
 
